90d7445a3c9f581e7e8b29e40d000ab59787f5ec
M1ttttw <matt_tay2003@outlook.com>
2024-06-17 19:14:13 -0400

    Revert "This shit ain't working"
    This reverts commit f38f2a9b93e8f386fa6d9a11b681cba628e93cde.

--- Assets/Scripts/UI/TransitionManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TransitionManager : MonoBehaviour
-{
-    public ShopManager shopManager;
-    public GridManager gridManager;
-}
