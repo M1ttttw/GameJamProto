f649b9e46bdd83edfa7b4d497bd3c8338f304b7e
M1ttttw <matt_tay2003@outlook.com>
2024-06-18 14:52:28 -0400

    Added the new changes from scene transition to main
    However...
    
    You will have to set up manager again yourself. This shouldn't be too hard to do, since it's the only object that isn't there in main. I also plan to add the startLevel script to be part of a button in the shopUI.

---
+++ Assets/Scripts/UI/Start Level/StartButton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour
+{
+    public Button startButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(SceneTransition);
+    }
+    void SceneTransition(){
+        SceneManager.LoadScene("FirstLevel", LoadSceneMode.Single);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
