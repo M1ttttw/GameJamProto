b640755d5664de2c7a35508375cfb6ca134ee6a9
M1ttttw <matt_tay2003@outlook.com>
2024-06-19 07:45:45 -0400

    Added a main menu button

--- Assets/Scripts/UI/GameOverScreen.cs
+++ Assets/Scripts/UI/GameOverScreen.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class GameOverScreen : MonoBehaviour
 {
+    public void mainMenu() { SceneManager.LoadScene("Start"); }
     public void playAgain() { SceneManager.LoadScene(SceneManager.GetActiveScene().name); }
-
     public void quitGame() { Application.Quit(); }
 }
