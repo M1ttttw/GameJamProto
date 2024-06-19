91d46e7724881fa1fe326e3d18a125380e5a78f8
M1ttttw <matt_tay2003@outlook.com>
2024-06-19 00:25:20 -0400

    Added a new script for scrolling.

---
+++ Assets/Scripts/UI/Background/Scrolling.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrolling : MonoBehaviour
+{
+    public float scrollSpeed;
+    public bool startScroll;
+
+    private Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        if (startScroll)
+        {
+            float y = Time.deltaTime * -scrollSpeed;
+            Vector2 currOffset = rend.sharedMaterial.GetTextureOffset("_MainTex");
+            Vector2 newOffset = currOffset + new Vector2(0, y);
+            rend.sharedMaterial.SetTextureOffset("_MainTex", newOffset);
+
+        }
+    }
+}
