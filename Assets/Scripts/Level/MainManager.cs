4f78a4ac3d1b968880c960b95991f632fb2f06ef
M1ttttw <matt_tay2003@outlook.com>
2024-06-19 03:53:20 -0400

    Removed and added some scripts

--- Assets/Scripts/Level/MainManager.cs
+++ Assets/Scripts/Level/MainManager.cs
@@ -4,33 +4,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Main : MonoBehaviour
+public class MainManager : MonoBehaviour
 {
-    public GameEvent startEvent;
+    public GameEvent onCarDeath;
     public GameEvent placementComplete;
-    private GameEventListener placementCompleteListener;
+    public GameEvent nextLevel;
+    public GameObject spawnAnchor;
     public GameEvent SceneLoaded;
     public bool rangeVisible;
     public GameObject enemy;
     public GameObject fastEnemy;
     public GameObject tankyEnemy;
+    public int enemyDeathScore;
     public float[] enemySpawnRate = {1,0,0};
     public int tankyCost = 50;
     public int fastCost = 5;
     private int lvlCost;
+    private int tempLC;
     public int level;
+    public GameObject carParent;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        placementCompleteListener = this.gameObject.GetComponent<GameEventListener>();
-        placementComplete.AddListener(placementCompleteListener);
-        // SceneLoaded.TriggerEvent();
+        carParent = GameObject.Find("Cars");
     }
 
     public void StartLevel(){
         SpawnWave(level);
+    }
+    public int CalculateReward(int x){
+        return x*50+100;
     }
     public int CalculateLvlPts(int x){
         return Mathf.FloorToInt(Mathf.Sqrt(x*500f));
@@ -61,9 +67,21 @@ public class Main : MonoBehaviour
         enemySpawnRate[1] = c2;
         enemySpawnRate[2] = c3;
     }
+    public void OnCarDeath(){
+        if (carParent.transform.childCount < 1){
+            SceneManager.LoadScene("GameOver", LoadSceneMode.Single);
+        }
+    }
+    public void onEnemyDeath(){
+        if (enemyDeathScore>= tempLC){
+            level +=1;
+            nextLevel.TriggerEvent();
+        }
+    }
     public void SpawnWave(int lvl){
         CalculateSpawnRate(lvl);
         lvlCost = CalculateLvlPts(lvl);
+        tempLC = lvlCost;
         StartCoroutine(SampleLevel());
     }
     IEnumerator SampleLevel()
@@ -72,6 +90,8 @@ public class Main : MonoBehaviour
         while (lvlCost>0){
             yield return new WaitForSeconds(UnityEngine.Random.Range(3f,5f));
             float temp = UnityEngine.Random.Range(0f,1f);
+            float x = spawnAnchor.transform.position.x;
+            float y = spawnAnchor.transform.position.y;
             Debug.Log(lvlCost);
             Debug.Log(temp);
             foreach  (float e in enemySpawnRate){
@@ -79,13 +99,13 @@ public class Main : MonoBehaviour
             }
             
             if (temp<enemySpawnRate[0] && lvlCost>= 50){
-                Instantiate(tankyEnemy, new Vector3(UnityEngine.Random.Range(-13f,9f), 11, 0), Quaternion.identity);
+                Instantiate(tankyEnemy, new Vector3(UnityEngine.Random.Range(x-12f,x+12f), y, 0), Quaternion.identity);
                 lvlCost -= 50;
             }else if(temp < enemySpawnRate[1] && lvlCost >= 5){
-                Instantiate(fastEnemy, new Vector3(UnityEngine.Random.Range(-13f,9f), 11, 0), Quaternion.identity);
+                Instantiate(fastEnemy, new Vector3(UnityEngine.Random.Range(x-12f,x+12f), y, 0), Quaternion.identity);
                 lvlCost -= 5;
             }else{
-                Instantiate(enemy, new Vector3(UnityEngine.Random.Range(-13f,9f), 11, 0), Quaternion.identity);
+                Instantiate(enemy, new Vector3(UnityEngine.Random.Range(x-12f,x+12f), y, 0), Quaternion.identity);
                 lvlCost -= 1;
             }
             
